215c2dd64c2f90b697410678db38a3c8e95c0db3
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-02-01 17:11:17 -0600

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -178,13 +178,13 @@ const DOM = {
 DOM.form.addEventListener('submit', sendMessage);
 
 function sendMessage() {
-  const swearlist=["sex","fuck","bitch","balls","cock","penis","porn","ass","dumbass","retard","cubs","pussy","segs","puusy","cub","dickhead","dick","shit","suck","retarded"];
+  const swearlist=["sex","fuck","bitch","balls","cock","penis","porn","ass","dumbass","retard","cubs","pussy","segs","puusy","cub","dickhead","dick","shit","suck","retarded","hi"];
   const value = DOM.input.value;
   const hasWord = (str, word) => 
   	str.replace(/[ .,  \   /#!    $%     \^&\*@;:{}='"?><+\-_`~(|)]/g,"").split(/\s+/).includes(word);
   
   var x=0;
-  var xmax=20;
+  var xmax=swearlist.length;
   var swearing=false;
   while (x<xmax){
   	var swear=hasWord(value,swearlist[x]);
