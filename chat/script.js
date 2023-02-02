4fe6b8f6e69857ff03960097ba022225ac4ec27f
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-02-02 16:43:51 -0600

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -240,8 +240,12 @@ function sendMessage() {
 	sweartest=value1.toLowerCase();
 	value1=sweartest.replace(swearlist[x], "####");
 	var sweartest2=value1.replace(" ","");
+	sweartest2=value1.replace(":","");\
+	var sweartest2=value1.replace(";","");
+	var sweartest2=value1.replace("!","");
+	var sweartest2=value1.replace(/[ .,  \   /#!    $%     \^&       \*@;:{}='"?><+\-_`~(|)]/g,"");
 	if (sweartest2.includes(swearlist[x])){
-		value1="################";
+		value1="########";
 	}
 	x=x+1;
   }
